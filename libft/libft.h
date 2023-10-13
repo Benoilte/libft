caff6fc751315d9d01e090ceb59b3e97e2cd0e42
benoit brandt <benoit.07@hotmail.com>
2023-10-13 13:32:50 +0200

    create function ft_isascii

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:42:12 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/13 12:01:40 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/13 13:24:10 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 #define LIBFT_H
 
 int	ft_atoi(char *str);
-int ft_isalnum(int x);
-int ft_isalpha(int x);
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
 
 #endif
