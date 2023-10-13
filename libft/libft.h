da5c140f47bada3920fcf04a89b1ceac0b8bcf85
benoit brandt <benoit.07@hotmail.com>
2023-10-13 18:23:58 +0200

    create strlcpy

--- libft/libft.h
+++ libft/libft.h
@@ -6,19 +6,20 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:42:12 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/13 16:13:45 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/13 17:29:42 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 #define LIBFT_H
 
-int	  ft_atoi(char *str);
-int   ft_isalnum(int c);
-int   ft_isalpha(int c);
-int   ft_isascii(int c);
-int   ft_isdigit(int c);
-int   ft_isprint(int c);
-char  *ft_strchr(const char *str, int c);
+int	    ft_atoi(char *str);
+int     ft_isalnum(int c);
+int     ft_isalpha(int c);
+int     ft_isascii(int c);
+int     ft_isdigit(int c);
+int     ft_isprint(int c);
+char    *ft_strchr(const char *str, int c);
+size_t  ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
