19eb95496eae845c95ea897e3f2883ba1e87148f
Bebrandt <benoit.07@hotmail.com>
2023-10-16 11:55:41 +0200

    create ft-strncmp

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:42:12 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/16 10:41:40 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/16 11:26:16 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int     ft_isascii(int c);
 int     ft_isdigit(int c);
 int     ft_isprint(int c);
 int		ft_strlen(char *str);
+int		ft_strncmp(const char *s1, const char *s2, long unsigned int n);
 char    *ft_strchr(const char *str, int c);
 size_t  ft_strlcpy(char *dest, const char *src, size_t size);
 
