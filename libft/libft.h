8b4274b8b0b345fc1d52f96cc4a86be4faee6867
Bebrandt <benoit.07@hotmail.com>
2023-10-16 19:02:39 +0200

    create memset function

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:42:12 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/16 16:26:02 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/16 18:02:08 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ char	*ft_strrchr(const char *str, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 size_t  ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+void 	*ft_memset(void *str, int c, size_t n);
 
 #endif
