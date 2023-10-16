6bb8713fb09fbc2d2b820bb6ae0af9f380eebcec
Bebrandt <benoit.07@hotmail.com>
2023-10-16 14:48:02 +0200

    create ft_toupper function

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:42:12 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/16 14:28:56 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:45:30 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int     ft_isascii(int c);
 int     ft_isdigit(int c);
 int     ft_isprint(int c);
 int		ft_tolower(int c);
+int		ft_toupper(int c);
 int		ft_strlen(char *str);
 int		ft_strncmp(const char *s1, const char *s2, long unsigned int n);
 char    *ft_strchr(const char *str, int c);
