6a427f5821417fd8d7d7f149db896d081079b80f
Bebrandt <benoit.07@hotmail.com>
2023-10-17 13:41:05 +0200

    create function memcmp

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:42:12 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/17 12:27:07 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/17 13:28:43 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 void 	*ft_memset(void *str, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+int 	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
