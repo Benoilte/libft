e7c2d9184efbe0b9029674d176eb9f2084f29364
benoit brandt <benoit.07@hotmail.com>
2023-10-21 14:27:51 +0200

    add strlcat test in comment because of windoes

--- test_libft/test_ft_strlcat.c
+++ test_libft/test_ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:04:43 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/20 11:40:28 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/21 13:39:22 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 int main(int argc, char **argv)
 {
-    int		r1;
+    // int		r1;
     int		r2;
-    char	buffer1[ft_atoi(argv[3])];
+    // char	buffer1[ft_atoi(argv[3])];
     char	buffer2[ft_atoi(argv[3])];
-	char	dest1[10] = "a";
+	// char	dest1[10] = "a";
 	char	dest2[10] = "a";
 
-	puts("------ STRLCAT -------");
-    printf("   STRLCAT: %lu\n", strlcat(dest1, "lorem ipsum dolor sit amet", 6));
-    write(1, "\n", 1);
-    write(1, dest1, 15);
-	write(1, "\n", 1);
+	// puts("------ STRLCAT -------");
+    // printf("   STRLCAT: %lu\n", strlcat(dest1, "lorem ipsum dolor sit amet", 6));
+    // write(1, "\n", 1);
+    // write(1, dest1, 15);
+	// write(1, "\n", 1);
 	puts("----- FT_STRLCAT -----");
     printf("FT_STRLCAT: %zu\n", ft_strlcat(dest2, "lorem ipsum dolor sit amet", 6));
     write(1, "\n", 1);
@@ -34,12 +34,12 @@ int main(int argc, char **argv)
 	write(1, "\n", 1);
 	if (argc == 4)
 	{
-		strcpy(buffer1, argv[1]);
-		r1 = strlcat(buffer1, argv[2], ft_atoi(argv[3]));
+		// strcpy(buffer1, argv[1]);
+		// r1 = strlcat(buffer1, argv[2], ft_atoi(argv[3]));
 		strcpy(buffer2, argv[1]);
 		r2 = ft_strlcat(buffer2, argv[2], ft_atoi(argv[3]));
-		puts("------ STRLCAT -------");
-		printf(" first str: '%s' \n append second str: '%s' \n result : '%s' \n length %d\n", argv[1], argv[2], buffer1, r1 );
+		// puts("------ STRLCAT -------");
+		// printf(" first str: '%s' \n append second str: '%s' \n result : '%s' \n length %d\n", argv[1], argv[2], buffer1, r1 );
 		puts("");
 		puts("");
 		puts("----- FT_STRLCAT -----");
