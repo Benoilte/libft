49d1a83ffdab23d57cedd91181df869452b75ef2
benoit brandt <benoit.07@hotmail.com>
2023-10-21 17:22:38 +0200

    create main for lstadd_front

--- test_libft/test_ft_lstadd_front_bonus.c
+++ test_libft/test_ft_lstadd_front_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_ft_lstnew_bonus.c                             :+:      :+:    :+:   */
+/*   test_ft_lstadd_front_bonus.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/21 14:52:06 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/21 17:20:26 by bebrandt         ###   ########.fr       */
+/*   Created: 2023/10/21 15:38:23 by bebrandt          #+#    #+#             */
+/*   Updated: 2023/10/21 17:20:18 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,5 @@
 
 int	main(void)
 {
-	int		content;
-	t_list	*node;
-
-	content = 42;
-	node = ft_lstnew(&content);
-	printf("content: %p, next: %p\n", node->content, node->next);
+	
 }
