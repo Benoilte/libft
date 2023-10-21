49329cdf2866714e20cce37d7bf4b8ff8821b6e3
benoit brandt <benoit.07@hotmail.com>
2023-10-21 15:25:47 +0200

    create test for lstnew

---
+++ test_libft/test_ft_lstnew_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_lstnew_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 14:52:06 by bebrandt          #+#    #+#             */
+/*   Updated: 2023/10/21 15:21:53 by bebrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+
+int	main(void)
+{
+	int		content;
+	t_list	*node;
+
+	content = 42;
+	node = ft_lstnew(&content);
+	printf("content: %p, next: %p\n", node->content, node->next);
+
+}
