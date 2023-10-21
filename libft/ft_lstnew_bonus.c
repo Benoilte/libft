62b50e770c08fba3094467822d3b290115bd7d44
benoit brandt <benoit.07@hotmail.com>
2023-10-21 15:25:09 +0200

    create ft_lstnew_bonus.c

---
+++ libft/ft_lstnew_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 14:42:40 by bebrandt          #+#    #+#             */
+/*   Updated: 2023/10/21 15:24:03 by bebrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* create a new t_list struct with malloc. assign var member content with
+params 'content' and assign var member next to NULL */
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+ 	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return ((void *)0);
+	new->content = content;
+	new->next = (void *)0;
+	return (new);
+}
