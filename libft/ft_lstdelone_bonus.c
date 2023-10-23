3493bfbda1a70e950d8237682dc6a400ff659ef4
Bebrandt <benoit.07@hotmail.com>
2023-10-23 11:48:54 +0200

    create ft_lstdelone

---
+++ libft/ft_lstdelone_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 08:44:59 by bebrandt          #+#    #+#             */
+/*   Updated: 2023/10/23 11:44:25 by bebrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*use del function to delete element's content and free lst memory*/
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst)
+		return ;
+	del(lst->content);
+	free(lst);
+}
